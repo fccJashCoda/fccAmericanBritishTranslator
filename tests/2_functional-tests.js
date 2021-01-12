8b0ebb52e311f6a6bbd88ebf8539b066561bbffd
fccJashCoda <personalrevolution2014@gmail.com>
2021-01-13 00:45:49 +0100

    added a suite wrapper to functional tests due to an issue with freeCodeCamp tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,107 +8,109 @@ chai.use(chaiHttp);
 const Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
-  test('Translation with text and locale fields: POST request to /api/translate', function (done) {
-    chai
-      .request(server)
-      .post('/api/translate')
-      .send({
-        text: 'Mangoes are my favorite fruit.',
-        locale: 'american-to-british',
-      })
-      .end(function (err, res) {
-        assert.equal(res.status, 200);
-        assert.isObject(res.body);
-        assert.deepEqual(res.body, {
+  suite('/api/translate tests', () => {
+    test('Translation with text and locale fields: POST request to /api/translate ->', function (done) {
+      chai
+        .request(server)
+        .post('/api/translate')
+        .send({
           text: 'Mangoes are my favorite fruit.',
-          translation:
-            'Mangoes are my <span class="highlight">favourite</span> fruit.',
+          locale: 'american-to-british',
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body);
+          assert.deepEqual(res.body, {
+            text: 'Mangoes are my favorite fruit.',
+            translation:
+              'Mangoes are my <span class="highlight">favourite</span> fruit.',
+          });
+          done();
         });
-        done();
-      });
-  });
-  test('Translation with text and invalid locale field: POST request to /api/translate', function (done) {
-    chai
-      .request(server)
-      .post('/api/translate')
-      .send({
-        text: 'Mangoes are my favorite fruit.',
-        locale: 'japanese-to-british',
-      })
-      .end(function (err, res) {
-        assert.equal(res.status, 200);
-        assert.isObject(res.body);
-        assert.deepEqual(res.body, {
-          error: 'Invalid value for locale field',
+    });
+    test('Translation with text and invalid locale field: POST request to /api/translate ->', function (done) {
+      chai
+        .request(server)
+        .post('/api/translate')
+        .send({
+          text: 'Mangoes are my favorite fruit.',
+          locale: 'japanese-to-british',
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body);
+          assert.deepEqual(res.body, {
+            error: 'Invalid value for locale field',
+          });
+          done();
         });
-        done();
-      });
-  });
-  test('Translation with missing text field: POST request to /api/translate', function (done) {
-    chai
-      .request(server)
-      .post('/api/translate')
-      .send({
-        locale: 'american-to-british',
-      })
-      .end(function (err, res) {
-        assert.equal(res.status, 200);
-        assert.isObject(res.body);
-        assert.deepEqual(res.body, {
-          error: 'Required field(s) missing',
+    });
+    test('Translation with missing text field: POST request to /api/translate ->', function (done) {
+      chai
+        .request(server)
+        .post('/api/translate')
+        .send({
+          locale: 'american-to-british',
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body);
+          assert.deepEqual(res.body, {
+            error: 'Required field(s) missing',
+          });
+          done();
         });
-        done();
-      });
-  });
-  test('Translation with missing locale field: POST request to /api/translate', function (done) {
-    chai
-      .request(server)
-      .post('/api/translate')
-      .send({
-        text: 'Mangoes are my favorite fruit.',
-      })
-      .end(function (err, res) {
-        assert.equal(res.status, 200);
-        assert.isObject(res.body);
-        assert.deepEqual(res.body, {
-          error: 'Required field(s) missing',
+    });
+    test('Translation with missing locale field: POST request to /api/translate ->', function (done) {
+      chai
+        .request(server)
+        .post('/api/translate')
+        .send({
+          text: 'Mangoes are my favorite fruit.',
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body);
+          assert.deepEqual(res.body, {
+            error: 'Required field(s) missing',
+          });
+          done();
         });
-        done();
-      });
-  });
-  test('Translation with empty text: POST request to /api/translate', function (done) {
-    chai
-      .request(server)
-      .post('/api/translate')
-      .send({
-        text: '',
-        locale: 'american-to-british',
-      })
-      .end(function (err, res) {
-        assert.equal(res.status, 200);
-        assert.isObject(res.body);
-        assert.deepEqual(res.body, {
-          error: 'No text to translate',
+    });
+    test('Translation with empty text: POST request to /api/translate ->', function (done) {
+      chai
+        .request(server)
+        .post('/api/translate')
+        .send({
+          text: '',
+          locale: 'american-to-british',
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body);
+          assert.deepEqual(res.body, {
+            error: 'No text to translate',
+          });
+          done();
         });
-        done();
-      });
-  });
-  test('Translation with text that needs no translation: POST request to /api/translate', function (done) {
-    chai
-      .request(server)
-      .post('/api/translate')
-      .send({
-        text: 'Mangoes are my favourite fruit.',
-        locale: 'american-to-british',
-      })
-      .end(function (err, res) {
-        assert.equal(res.status, 200);
-        assert.isObject(res.body);
-        assert.deepEqual(res.body, {
+    });
+    test('Translation with text that needs no translation: POST request to /api/translate ->', function (done) {
+      chai
+        .request(server)
+        .post('/api/translate')
+        .send({
           text: 'Mangoes are my favourite fruit.',
-          translation: 'Everything looks good to me!',
+          locale: 'american-to-british',
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body);
+          assert.deepEqual(res.body, {
+            text: 'Mangoes are my favourite fruit.',
+            translation: 'Everything looks good to me!',
+          });
+          done();
         });
-        done();
-      });
+    });
   });
 });
